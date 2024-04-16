fd724f3df1742bd298bde41464c52c560e32434a
Anthony Josué Rojas Fuentes <anthony.rf00@gmail.com>
2024-04-16 17:10:12 -0600

    Update README.md

--- SistemaInventario-cs-ASP.NET8CoreWebAppMVC/obj/Debug/net8.0/SistemaInventario-cs-ASP.NET8CoreWebAppMVC.AssemblyInfo.cs
+++ SistemaInventario-cs-ASP.NET8CoreWebAppMVC/obj/Debug/net8.0/SistemaInventario-cs-ASP.NET8CoreWebAppMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario-cs-ASP.NET8CoreWebAppMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032cb4f18d2c1308f7f52bb7add8f3a4882672d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario-cs-ASP.NET8CoreWebAppMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario-cs-ASP.NET8CoreWebAppMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
